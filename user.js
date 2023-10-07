b09d3e0fed5e807f89a6dc21b9f630931de9068a
Daniel <110414111+dKally@users.noreply.github.com>
2023-10-07 14:21:14 -0300

    Add files via upload

--- user.js
+++ user.js
@@ -7,93 +7,107 @@ const userJSON = path.join(__dirname, 'user.json')
 const divRegister = document.querySelector('.register')
 const appHtml = path.join(__dirname, '/src', '/index.html')
 
-fs.readFile(userJSON, 'utf8', (error, data)=>{
-    if(error){
-        console.log(error)
-        return
-    }
+let homeScreen = true
 
-    console.log(data)
-    const jsonObject = JSON.parse(data)
-    const user = jsonObject.user
+if(homeScreen === true){
+    document.addEventListener('keydown', (event)=>{
+        if(event.key){
+            document.querySelector('.welcome').classList.add('hide')
+            startLogin()
+        }
+    })
+}
 
-    if(user === ""){
-        // Criando usuário
-        divRegister.classList.remove('hide')
-      
-        document.querySelector('.send-register').addEventListener('click',()=>{
-        const userName = document.querySelector('.name-register').value
-        const password = document.querySelector('.password-register').value
-        const passwordConfirm = document.querySelector('.password-confirm-register').value
-
-        console.log(userName, password, passwordConfirm)
-
-        if(password !== passwordConfirm){
-            document.querySelector('.alert-password').classList.remove('hide')
-            setTimeout(() => {
-                document.querySelector('.alert-password').classList.add('hide')
-            }, 5000);
+function startLogin(){
+    fs.readFile(userJSON, 'utf8', (error, data)=>{
+        if(error){
+            console.log(error)
+            return
+        }
+    
+        console.log(data)
+        const jsonObject = JSON.parse(data)
+        const user = jsonObject.user
+    
+        if(user === ""){
+            // Criando usuário
+            divRegister.classList.remove('hide')
+          
+            document.querySelector('.send-register').addEventListener('click',()=>{
+            const userName = document.querySelector('.name-register').value
+            const password = document.querySelector('.password-register').value
+            const passwordConfirm = document.querySelector('.password-confirm-register').value
+    
+            console.log(userName, password, passwordConfirm)
+    
+            if(password !== passwordConfirm){
+                document.querySelector('.alert-password').classList.remove('hide')
+                setTimeout(() => {
+                    document.querySelector('.alert-password').classList.add('hide')
+                }, 5000);
+            }
+            else{
+                const user = `{ "user":{ "userName": ${userName}, "password": ${userName} }}`
+    
+                
+                console.log(user)
+    
+                fs.writeFile(userJSON, user, 'utf8', (error)=>{
+                    if(error){
+                        console.log(error)
+                    }else{
+                        console.log('Usuário criado com sucesso!')
+                        console.log(document.querySelector('.user-created'))
+                        document.querySelector('.user-created').classList.remove('hide')
+                        document.querySelector('.btn-user-created').addEventListener('click', ()=>{
+                            window.location.href = appHtml
+                        })
+                    }
+                })
+    
+            }
+          })
         }
         else{
-            const user = `{ "user":{ "userName": ${userName}, "password": ${userName} }}`
-
-            
-            console.log(user)
-
-            fs.writeFile(userJSON, user, 'utf8', (error)=>{
+            //Fazendo login
+            document.querySelector('.sign-in').classList.remove('hide')
+            fs.readFile(userJSON, 'utf8', (error, data)=>{
                 if(error){
                     console.log(error)
-                }else{
-                    console.log('Usuário criado com sucesso!')
-                    console.log(document.querySelector('.user-created'))
-                    document.querySelector('.user-created').classList.remove('hide')
-                    document.querySelector('.btn-user-created').addEventListener('click', ()=>{
+                    return
+                }
+                
+                console.log(data)
+                const jsonObject = JSON.parse(data)
+                const user = jsonObject.user
+                const userName = String(user.userName)
+                const password = String(user.password)
+    
+                console.log(userName, password)
+    
+                document.querySelector('.send-login').addEventListener('click', ()=>{
+                    const answerUserName = document.querySelector('.name-login').value
+                    const answerPassword = document.querySelector('.password-login').value
+    
+                    if(!answerPassword || answerUserName === ""){
+                        return console.log('Preencha todos os campos!')
+                    }
+                    console.log(answerUserName, answerPassword)
+    
+                    if(answerUserName === userName && answerPassword === password){
+                        
                         window.location.href = appHtml
-                    })
-                }
+                    }
+                    else{
+                        console.log('Senha errada!')
+                        document.querySelector('.alert-login').classList.remove('hide')
+                        setTimeout(() => {
+                            document.querySelector('.alert-login').classList.add('hide')
+                        }, 5000);
+                    }
+                })
             })
-
         }
-      })
-    }
-    else{
-        //Fazendo login
-        document.querySelector('.sign-in').classList.remove('hide')
-        fs.readFile(userJSON, 'utf8', (error, data)=>{
-            if(error){
-                console.log(error)
-                return
-            }
-            
-            console.log(data)
-            const jsonObject = JSON.parse(data)
-            const user = jsonObject.user
-            const userName = String(user.userName)
-            const password = String(user.password)
+    })
+}
 
-            console.log(userName, password)
-
-            document.querySelector('.send-login').addEventListener('click', ()=>{
-                const answerUserName = document.querySelector('.name-login').value
-                const answerPassword = document.querySelector('.password-login').value
-
-                if(!answerPassword || answerUserName === ""){
-                    return console.log('Preencha todos os campos!')
-                }
-                console.log(answerUserName, answerPassword)
-
-                if(answerUserName === userName && answerPassword === password){
-                    
-                    window.location.href = appHtml
-                }
-                else{
-                    console.log('Senha errada!')
-                    document.querySelector('.alert-login').classList.remove('hide')
-                    setTimeout(() => {
-                        document.querySelector('.alert-login').classList.add('hide')
-                    }, 5000);
-                }
-            })
-        })
-    }
-})
